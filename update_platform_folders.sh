02fc131fe864b70eb9943b6690676cbc0fc32c95
Connectety-T <connectetyGit@yandex.com>
2020-06-05 22:40:11 +0200

    update update_platform_folders.sh and resulting gitignore make update script: - exit on error - runnable via AndroidStudio - add custom files to freshly generated .gitignore
    update gitignore because we updated the content in the update script

--- update_platform_folders.sh
+++ update_platform_folders.sh
@@ -1,16 +1,20 @@
 #!/usr/bin/env bash
 
+set -e
+
 name="setup4bmatotp"
 org="io.github.connectety"
 desc="helps setting up a totp app to use as a second factor authenticator for blizzard"
 
-flutter pub upgrade
+~/development/flutter/bin/flutter pub upgrade
 
 rm .gitignore
 rm -rf linux macos windows
 
-flutter create --no-pub --project-name="$name" --org="$org" --description="$desc" .
+~/development/flutter/bin/flutter create --project-name="$name" --org="$org" --description="$desc" .
 
 rm test/widget_test.dart
+
+echo -e "\n# Users custom\n/.gradle/\n/analysis_options.yaml" >> .gitignore
 
 git add linux macos windows .gitignore
